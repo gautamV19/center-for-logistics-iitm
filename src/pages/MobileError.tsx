e873acb5b1b4db08efdb2735e3e39403c20520fc
gautamV19 <ee20b144@smail.iitm.ac.in>
2024-04-11 00:33:32 +0530

    mobile footer and mobile folder structure

---
+++ src/pages/MobileError.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MobileError() {
+  return <div>MobileError</div>;
+}
---
+++ src/pages/MobileInternships.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MobileInternship() {
+  return <div>MobileInternship</div>;
+}
